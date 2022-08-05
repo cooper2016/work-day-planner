ed2465a735dc61b906c1c9d1c84008bca3291ef0
hrcoop16 <hrcoop16@gmail.com>
2022-08-05 11:06:59 -0500

    html and css complete

--- assets/script.js
+++ assets/script.js
@@ -6,12 +6,40 @@ function init(){
     displayDate();
 }
 
-//Display current date
+//Display current date at the top of the planner
 function displayDate(){
     var today = moment().format('dddd, MMMM Do YYYY');
     timeDisplayEl.text(today);
 }
 
+//standard business hour time blocks displayed below
+
+
+//color code timeblocks based on past preset and future
+
+//Can type into time block to create an event
+
+//select save time block to save event in locale storage
+
+//make saved events banner come down
+
+
+
 
 
 init();
+
+
+// //array of times 
+
+// times = [
+//     '9 am', 
+//     '10 am',
+//     '11 am', 
+//     '12 pm', 
+//     '1 pm', 
+//     '2 pm', 
+//     '3 pm', 
+//     '4 pm',
+//     '5 pm'
+// ]
